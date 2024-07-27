46aed222dbbfdea96ac70cd51e64dadaa37cc5a9
Habibul Islam <habibulislam6862@gmail.com>
2024-07-27 12:04:14 +0600

    updated

--- class/NanoShortCodes.php
+++ class/NanoShortCodes.php
@@ -7,9 +7,14 @@ class CustomCompanyAddonNanoShortCodes
         self::get_review_quantities();
     }
 
+    /**
+     * Get Review description
+     * shortcode: [get_comments_number]
+     * @return void
+     */
     public static function get_review_quantities(){
         add_shortcode("get_comments_number", function(){
-            echo get_comments_number();
+            echo "<div class='review-description-header-custom-company-addon'>" . get_comments_number() . "</div>";
         });
     }
 }

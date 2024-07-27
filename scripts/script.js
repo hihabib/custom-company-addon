7ac4fa2622c021a7e3b82930db58b19d411bf426
Habibul Islam <habibulislam6862@gmail.com>
2024-07-27 12:15:07 +0600

    updated

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,33 @@
 class CustomCompanyAddon {
     constructor() {
         this.ratingIconBeautify();
+        this.addRatingTag();
     }
 
-    ratingIconBeautify(){
+    /**
+     * Rating Icons
+     * fontawesome i elements
+     * @returns {NodeListOf<Element>}
+     */
+    ratingIconsNodeList(){
         const iconsContainer = document.querySelector(".rating-stars");
-        const icons = iconsContainer.querySelectorAll('.fa.stars-style-solid');
-        // calculate total average rating
-        const ratingLength = Array.from(icons).reduce((acc, next) => next.classList.contains('rated') ? ++acc : acc, 0);
+        return iconsContainer.querySelectorAll('.fa.stars-style-solid');
+    }
+
+    /**
+     * Average rating length out of five;
+     * @returns {unknown}
+     */
+    averageRatingLength(){
+        const icons = this.ratingIconsNodeList();
+        return Array.from(icons).reduce((acc, next) => next.classList.contains('rated') ? ++acc : acc, 0);
+    }
+    /**
+     * Top rating beatification
+     */
+    ratingIconBeautify(){
+        const icons = this.ratingIconsNodeList();
+        const ratingLength = this.averageRatingLength();
         // add classes for styling.
         icons.forEach(icon => {
             const isRated = icon.classList.contains("rated")
@@ -21,6 +41,20 @@ class CustomCompanyAddon {
                 icon.classList.add('gray-rating');
             }
         })
+    }
+
+    addRatingTag(){
+        const ratingLength = this.averageRatingLength();
+        const reviewDescription =document.querySelector('.review-description-header-custom-company-addon');
+        if(ratingLength <= 2){
+            reviewDescription.innerText += "Below Average";
+        } else if(ratingLength <= 3){
+            reviewDescription.innerText += "Average";
+        } else if(ratingLength <= 4){
+            reviewDescription.innerText += "Great";
+        } else {
+            reviewDescription.innerText += "Excellent";
+        }
     }
 }
 

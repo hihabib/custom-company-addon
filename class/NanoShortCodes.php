b6e0d82931dabcb786b94f41710bdefa6519873f
Habibul Islam <habibulislam6862@gmail.com>
2024-08-01 03:07:29 +0600

    get_review_quantities method removed from CustomCompanyAddonNanoShortCodes class

--- class/NanoShortCodes.php
+++ class/NanoShortCodes.php
@@ -4,19 +4,7 @@ class CustomCompanyAddonNanoShortCodes
 {
     public function __construct()
     {
-        self::get_review_quantities();
         self::get_verification_badge();
-    }
-
-    /**
-     * Get Review description
-     * shortcode: [get_comments_number]
-     * @return void
-     */
-    public static function get_review_quantities(){
-        add_shortcode("get_comments_number", function(){
-            echo "<div class='review-description-header-custom-company-addon'> Reviews " . get_comments_number() . "</div>";
-        });
     }
 
     /**

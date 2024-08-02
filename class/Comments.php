258d87feea0dbd96a79a119ee5223c2db3dc2f1c
Habibul Islam <habibulislam6862@gmail.com>
2024-08-02 18:21:35 +0600

    updated

--- class/Comments.php
+++ class/Comments.php
@@ -1,14 +1,17 @@
 <?php
 
-class CustomCompanyAddonComment {
-    public function  __construct()
+class CustomCompanyAddonComment
+{
+    public function __construct()
     {
         add_filter('query_vars', [$this, 'custom_register_query_vars']);
-        add_action('init', function(){
-            $this -> comments_template();
+        add_action('init', function () {
+            $this->comments_template();
         });
     }
-    public function custom_register_query_vars($vars) {
+
+    public function custom_register_query_vars($vars)
+    {
         $vars[] = 'rating';
         return $vars;
     }
@@ -16,9 +19,8 @@ class CustomCompanyAddonComment {
     public function comments_template()
     {
         add_shortcode("custom_company_comments_template", function () {
-            $args = array();
+            $args = [];
             if (is_singular('company') && get_query_var('rating')) {
-                echo "works query fine";
                 $args = array(
                     'post_id' => get_the_ID(),
                     'meta_query' => array(
@@ -29,15 +31,11 @@ class CustomCompanyAddonComment {
                         )
                     )
                 );
-                $comment_query = new WP_Comment_Query($args);
-                $comments = $comment_query -> comments;
-                wp_list_comments([], $comments);
 
-            } else {
-                echo "works";
-                // Call wp_list_comments without filtering
-                wp_list_comments($args);
             }
+            $comment_query = new WP_Comment_Query($args);
+            $comments = $comment_query->comments;
+            wp_list_comments([], $comments);
         });
     }
 

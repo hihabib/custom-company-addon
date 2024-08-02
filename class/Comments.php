bed2729b71c6dcdf463f8ab583540381b4d2fca3
Habibul Islam <habibulislam6862@gmail.com>
2024-08-02 18:16:59 +0600

    updated

--- class/Comments.php
+++ class/Comments.php
@@ -4,10 +4,38 @@ class CustomCompanyAddonComment {
     public function  __construct()
     {
         add_filter('query_vars', [$this, 'custom_register_query_vars']);
+        $this -> comments_template();
     }
     public function custom_register_query_vars($vars) {
         $vars[] = 'rating';
         return $vars;
+    }
+
+    public function comments_template()
+    {
+        add_shortcode("custom_company_comments_template", function () {
+            $args = array();
+            if (is_singular('company') && get_query_var('rating')) {
+                echo "works";
+                $args = array(
+                    'post_id' => get_the_ID(),
+                    'meta_query' => array(
+                        array(
+                            'key' => 'rating',
+                            'value' => get_query_var('rating'),
+                            'compare' => '='
+                        )
+                    )
+                );
+                $comment_query = new WP_Comment_Query($args);
+                $comments = $comment_query -> comments;
+                wp_list_comments([], $comments);
+
+            } else {
+                // Call wp_list_comments without filtering
+                wp_list_comments($args);
+            }
+        });
     }
 
 }

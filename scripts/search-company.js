c86d22c7804a180df50978ed555c919956356532
Habibul Islam <habibulislam6862@gmail.com>
2024-08-02 20:50:30 +0600

    updated

--- scripts/search-company.js
+++ scripts/search-company.js
@@ -5,8 +5,10 @@
 const companySearchResult = document.querySelector('#company_search_result');
 
 let isCompanySearchResultHovered = false;
-companySearchResult.addEventListener('mouseover', () => isCompanySearchResultHovered = true);
-companySearchResult.addEventListener('mouseout', () => isCompanySearchResultHovered = false);
+if(companySearchResult !== null){
+    companySearchResult.addEventListener('mouseover', () => isCompanySearchResultHovered = true);
+    companySearchResult.addEventListener('mouseout', () => isCompanySearchResultHovered = false);
+}
 
 /**
  * @type {HTMLInputElement | null}
